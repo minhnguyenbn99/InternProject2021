83a9cfe99ca0b8eabc1c3c613afa484d03985be8
minhnguyenbn99 <66963269+minhnguyenbn99@users.noreply.github.com>
2021-08-23 23:05:29 +0700

    Lesson5

---
+++ MiniGameStarter/TrainingFramework/src/GameStates/GSCredit.cpp
@@ -0,0 +1,109 @@
+#include "GSCredit.h"
+#include "Camera.h"
+
+
+GSCredit::GSCredit() {
+}
+
+
+GSCredit::~GSCredit()
+{
+}
+
+
+
+void GSCredit::Init()
+{
+    auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D.nfg");
+    auto texture = ResourceManagers::GetInstance()->GetTexture("anh nen 2.tga");
+
+    // background
+    auto shader = ResourceManagers::GetInstance()->GetShader("TextureShader");
+    m_background = std::make_shared<Sprite2D>(model, shader, texture);
+    m_background->Set2DPosition(Globals::screenWidth / 2, Globals::screenHeight / 2);
+    m_background->SetSize(Globals::screenWidth, Globals::screenHeight);
+
+    // close button
+    texture = ResourceManagers::GetInstance()->GetTexture("btn_prev.tga");
+    std::shared_ptr<GameButton> buttonClose = std::make_shared<GameButton>(model, shader, texture);
+    buttonClose->Set2DPosition(Globals::screenWidth - 50, 50);
+    buttonClose->SetSize(50, 50);
+    buttonClose->SetOnClick([]()
+        {
+            GameStateMachine::GetInstance()->PopState();
+        });
+    m_listButton.push_back(buttonClose);
+
+    // Form Infor
+    texture = ResourceManagers::GetInstance()->GetTexture("gs credit.tga");
+    m_panelCredits = std::make_shared<Sprite2D>(model, shader, texture);
+    m_panelCredits->Set2DPosition(Globals::screenWidth / 2, 300);
+    m_panelCredits->SetSize(450, 250);
+
+    // credit text
+    shader = ResourceManagers::GetInstance()->GetShader("TextShader");
+    std::shared_ptr<Font> font = ResourceManagers::GetInstance()->GetFont("arialbd.ttf");
+    m_textGameName = std::make_shared< Text>(shader, font, "Nguyen Duc Minh - Hust", TextColor:: BLACK, 1.0f);
+    m_textGameName1 = std::make_shared< Text>(shader, font, "Shooting Game", TextColor::BLACK, 1.0f);
+    m_textGameName->Set2DPosition(Vector2(80, 300));
+    m_textGameName1->Set2DPosition(Vector2(80, 330));
+}
+
+void GSCredit::Exit()
+{
+}
+
+
+void GSCredit::Pause()
+{
+}
+
+void GSCredit::Resume()
+{
+}
+
+
+void GSCredit::HandleEvents()
+{
+}
+
+void GSCredit::HandleKeyEvents(int key, bool bIsPressed)
+{
+}
+
+void GSCredit::HandleTouchEvents(int x, int y, bool bIsPressed)
+{
+    for (auto button : m_listButton)
+    {
+        if (button->HandleTouchEvents(x, y, bIsPressed))
+        {
+            break;
+        }
+    }
+}
+
+void GSCredit::HandleMouseMoveEvents(int x, int y)
+{
+}
+
+void GSCredit::Update(float deltaTime)
+{
+    m_background->Update(deltaTime);
+    for (auto it : m_listButton)
+    {
+        it->Update(deltaTime);
+    }
+}
+
+void GSCredit::Draw()
+{
+    m_background->Draw();
+    m_panelCredits->Draw();
+    m_textGameName->Draw();
+    m_textGameName1->Draw();
+
+    for (auto it : m_listButton)
+    {
+        it->Draw();
+    }
+}
---
+++ MiniGameStarter/TrainingFramework/src/GameStates/GSMenu.cpp
@@ -0,0 +1,119 @@
+#include "GSMenu.h"
+#include "Camera.h"
+
+GSMenu::GSMenu() : GameStateBase(StateType::STATE_MENU), 
+	m_background(nullptr), m_listButton(std::list<std::shared_ptr<GameButton>>{}), m_textGameName(nullptr)
+{
+}
+
+
+GSMenu::~GSMenu()
+{
+}
+
+
+
+void GSMenu::Init()
+{
+	auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D.nfg");
+	auto texture = ResourceManagers::GetInstance()->GetTexture("anh bg.tga");
+
+	// background
+	auto shader = ResourceManagers::GetInstance()->GetShader("TextureShader");
+	m_background = std::make_shared<Sprite2D>(model, shader, texture);
+	m_background->Set2DPosition(Globals::screenWidth / 2, Globals::screenHeight / 2);
+	m_background->SetSize(Globals::screenWidth, Globals::screenHeight);
+
+	// play button
+	texture = ResourceManagers::GetInstance()->GetTexture("Play1.tga");
+	std::shared_ptr<GameButton> button = std::make_shared<GameButton>(model, shader, texture);
+	button->Set2DPosition(Globals::screenWidth / 2, Globals::screenHeight / 2);
+	button->SetSize(100, 100);
+	button->SetOnClick([]() {
+		GameStateMachine::GetInstance()->ChangeState(StateType::STATE_PLAY);
+		});
+	m_listButton.push_back(button);
+
+
+	// GS Credit
+	texture = ResourceManagers::GetInstance()->GetTexture("anh credit.tga");
+	std::shared_ptr<GameButton> buttoncredit = std::make_shared<GameButton>(model, shader, texture);
+	buttoncredit->Set2DPosition(Globals::screenWidth / 2, Globals::screenHeight / 2 + 200);
+	buttoncredit->SetSize(100, 100);
+	buttoncredit->SetOnClick([]() {
+		GameStateMachine::GetInstance()->ChangeState(StateType::STATE_CREDIT);
+		});
+	m_listButton.push_back(buttoncredit);
+
+	// exit button
+	texture = ResourceManagers::GetInstance()->GetTexture("btn_close.tga");
+	button = std::make_shared<GameButton>(model, shader, texture);
+	button->Set2DPosition(Globals::screenWidth - 50, 50);
+	button->SetSize(50, 50);
+	button->SetOnClick([]() {
+		exit(0);
+		});
+	m_listButton.push_back(button);
+
+	// game title
+	shader = ResourceManagers::GetInstance()->GetShader("TextShader");
+	std::shared_ptr<Font> font = ResourceManagers::GetInstance()->GetFont("arialbd.ttf");
+	m_textGameName = std::make_shared< Text>(shader, font, "SHOOTING GAME", TextColor::BLUE, 1.5f);
+	m_textGameName->Set2DPosition(Vector2(80, 200));
+}
+
+void GSMenu::Exit()
+{
+}
+
+
+void GSMenu::Pause()
+{
+}
+
+void GSMenu::Resume()
+{
+}
+
+
+void GSMenu::HandleEvents()
+{
+}
+
+void GSMenu::HandleKeyEvents(int key, bool bIsPressed)
+{
+}
+
+void GSMenu::HandleTouchEvents(int x, int y, bool bIsPressed)
+{
+	for (auto button : m_listButton)
+	{
+		if (button->HandleTouchEvents(x, y, bIsPressed))
+		{
+			break;
+		}
+	}
+}
+
+void GSMenu::HandleMouseMoveEvents(int x, int y)
+{
+}
+
+void GSMenu::Update(float deltaTime)
+{
+	m_background->Update(deltaTime);
+	for (auto it : m_listButton)
+	{
+		it->Update(deltaTime);
+	}
+}
+
+void GSMenu::Draw()
+{
+	m_background->Draw();
+	for (auto it : m_listButton)
+	{
+		it->Draw();
+	}
+	m_textGameName->Draw();
+}
